01176669c1b30b1e1b6abc565088feb05bf0bdd7
Davide Maiorana <maiordavid.dm@gmail.com>
2026-04-30 11:36:18 +0200

    Initial Commit

---
+++ MayoBG/MayoBGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MayoBGApp.swift
+//  MayoBG
+//
+//  Created by Davide Maiorana on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MayoBGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
